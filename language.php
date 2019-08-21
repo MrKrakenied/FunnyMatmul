b51b95bc17af4b6b8a0eb4abd00f9f404d194a44
Dzikoysk <dzikoysk@dzikoysk.net>
2019-08-22 01:06:56 +0200

    Update language.php

--- language.php
+++ language.php
@@ -1,1 +1,3 @@
-we need to declare the language of repository to get a nice color and php looks almost as powerful as skript
+<?php
+   // we need to declare the language of repository to get a nice color and php looks almost as powerful as skript
+?>
