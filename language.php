244df1d382aa7ee69d48ef275a1d2ce446034ac0
Dzikoysk <dzikoysk@dzikoysk.net>
2019-08-22 01:08:30 +0200

    Update language.php

--- language.php
+++ language.php
@@ -1,3 +1,4 @@
 <?php
    // we need to declare the language of repository to get a nice color and php looks almost as powerful as skript
+   echo "not hack"; // due to ~ https://en.wikipedia.org/wiki/Hack_(programming_language)
 ?>
